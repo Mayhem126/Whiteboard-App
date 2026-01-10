723035c9cd7ab131115746098c3503322684b91a
Swastik <kitsaws12@gmail.com>
2026-01-11 02:36:22 +0530

    setup frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Board from "./Components/Board"
+import Toolbar from "./Components/Toolbar"
+import Toolbox from "./Components/Toolbox"
+import BoardProvider from "./store/BoardProvider"
+import ToolboxProvider from "./store/ToolboxProvider"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BoardProvider>
+      <ToolboxProvider>
+        <Toolbar />
+        <Board />
+        <Toolbox />
+      </ToolboxProvider>
+    </BoardProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ src/store/board-context.js
@@ -0,0 +1,15 @@
+import { createContext } from "react"
+
+const boardContext = createContext({
+  activeToolItem: "",
+  toolActionType: "",
+  elements: [],
+  history:[[]],
+  index: 0,
+  boardMouseDownHandler: () => {},
+  boardMouseMoveHandler: () => {},
+  boardMouseUpHandler: () => {},
+  changeToolHandler: () => {},
+})
+
+export default boardContext
