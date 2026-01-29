5aff0bb96e4c29ca2dc4b6ba82e42672b69da33e
Swastik <kitsaws12@gmail.com>
2026-01-29 11:37:51 +0530

    fix PORT

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 const app = require("./app")
-const { PORT } = require("./config/env")
+const PORT = process.env.PORT || 5000
 const connectToDatabase = require("./config/db")
 
 connectToDatabase()
@@ -11,7 +11,11 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      process.env.FRONTEND_URL,
+    ],
     methods: ["GET", "POST"],
   },
 })
